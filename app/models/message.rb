691eeb0932b2efff076d7182bb4b8b7520fb3d45
Jay Lin <jay101630@gmail.com>
2016-12-25 15:11:42 +0800

    fix delete bug

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,6 @@ class Message < ActiveRecord::Base
     belongs_to :user
     belongs_to :announce
 
-    has_many :replies, class_name: "Message", foreign_key: "message_id"
+    has_many :replies, class_name: "Message", foreign_key: "message_id", dependent: :destroy
     belongs_to :message, class_name: "Message"
 end
