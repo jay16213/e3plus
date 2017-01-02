17069069c76b280117c7fc9ed0262cb577b2ec8c
8igMac <michaelshih123@gmail.com>
2017-01-02 22:23:32 +0800

    include javascript and css file to student layout

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,3 +11,6 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( main.css )
 Rails.application.config.assets.precompile += %w( main.js )
+Rails.application.config.assets.precompile += %w( student.scss )
+Rails.application.config.assets.precompile += %w( student.coffee )
+
