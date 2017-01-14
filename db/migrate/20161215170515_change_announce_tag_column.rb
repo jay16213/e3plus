0a003b54c9d0b5c80f0dc93482af59a6d6e893cf
Jay Lin <jay101630@gmail.com>
2017-01-15 00:31:01 +0800

    fix migration bug

--- db/migrate/20161215170515_change_announce_tag_column.rb
+++ db/migrate/20161215170515_change_announce_tag_column.rb
@@ -1,6 +1,6 @@
 class ChangeAnnounceTagColumn < ActiveRecord::Migration
   def change
     rename_column :announces, :tag, :tag_id
-    change_column :announces, :tag_id, :integer
+    change_column :announces, :tag_id, :integer, 'integer USING CAST(:tag_id AS integer)'
   end
 end
